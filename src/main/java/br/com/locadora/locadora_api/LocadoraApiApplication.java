bb94e88e75b277e7a2b803d2981da432f95dda73
c093691 <c093691@corp.caixa.gov.br>
2026-05-02 17:12:24 -0300

    primeiro commit na main

---
+++ src/main/java/br/com/locadora/locadora_api/LocadoraApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.locadora.locadora_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraApiApplication.class, args);
+	}
+
+}
