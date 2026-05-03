5b0f560d750bbfa208d03984d0b4bc435f79ad76
c093691 <c093691@corp.caixa.gov.br>
2026-05-02 21:41:04 -0300

    ajuste na entity Cliente.java

--- src/main/java/br/com/locadora/locadora_api/entity/Cliente.java
+++ src/main/java/br/com/locadora/locadora_api/entity/Cliente.java
@@ -23,43 +23,63 @@ public class Cliente {
 
 
     public Long getId() {
+
         return id;
+
     }
 
     public void setId(Long id) {
+
         this.id = id;
+
     }
 
     public String getCpf() {
+
         return cpf;
+
     }
 
     public void setCpf(String cpf) {
+
         this.cpf = cpf;
+
     }
 
     public String getNome() {
+
         return nome;
+
     }
 
     public void setNome(String nome) {
+
         this.nome = nome;
+
     }
 
     public String getEndereco() {
+
         return endereco;
+
     }
 
     public void setEndereco(String endereco) {
+
         this.endereco = endereco;
+
     }
 
     public String getCidade() {
+
         return cidade;
+
     }
 
     public void setCidade(String cidade) {
+
         this.cidade = cidade;
+
     }
 }
 
